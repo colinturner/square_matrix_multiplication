9c6b6c6129a86a6b748cfddfa6d5e09853fa9c42
Colin Turner <colinturner92@gmail.com>
2017-03-07 17:13:05 +0000

    first test

---
+++ lib/sq_mat_mult.rb
@@ -0,0 +1,6 @@
+def sq_mat_mult(a, b)
+  z = []
+  prod = a[0]*b[0]
+  z.push(prod)
+  z
+end
--- spec/sq_mat_mult_spec.rb
+++ spec/sq_mat_mult_spec.rb
@@ -1,5 +1,11 @@
+require 'sq_mat_mult'
+
 describe 'square_matrix_multiplication' do
   it 'returns [1] when "a" and "b" are both [1] ' do
     expect(sq_mat_mult([1],[1])).to eq [1]
+  end
+
+  it 'returns [[5, 4], [11, 8]]' do
+    expect(sq_mat_mult([[1, 2], [3, 2]], [[3, 2], [1, 1]])). to eq [[5, 4], [11, 8]]
   end
 end
